9522c909497f0737de75e0c30acb634ce16840e9
Nathan louissaint <louissaintmark@gmail.com>
2023-03-01 08:49:07 -0500

    more functions and false values

---
+++ more.js
@@ -0,0 +1,22 @@
+// lower case method
+let text = "Hello World!";
+let result = text.toLowerCase();
+console.log(result)
+
+let word = "Hello"
+let word2 = "Nathan"
+let results = word.concat(word2)
+console.log(results)
+// new instance
+class Shovel {
+    constructor(type,price){
+        this._type = type;
+        this._price = price
+    }
+}
+const myShovel = new Shovel('snow',20)
+console.log(myShovel)
+
+//flase values
+const flasyvalue1 = false;
+console.log(flasyvalue1)
