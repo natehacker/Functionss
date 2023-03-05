765d605da11709056611775b2266d1590ef49292
Nathan louissaint <louissaintmark@gmail.com>
2023-03-04 19:14:22 -0500

    nathan louissaint muscle memory coding

--- new.js
+++ new.js
@@ -11,3 +11,20 @@ console.log(num.toFixed(5))
 nathan(5)
 
 // adding all the numbers in an array 
+
+let num = [1,2,3,4,5]
+// The output should be 15
+let sum = num.reduce((accumlator,currentValue)=>{
+  return accumlator + currentValue
+}
+,0)
+console.log(sum)
+// lets go we did it 
+// now do the same thing but multiply all the numbers in the array by 2
+let newNum = [1,2,3,4,5]
+let Newsum = newNum.map(double)
+function double(value){
+return value * 2
+}
+console.log(Newsum)
+// lets go we did it again
